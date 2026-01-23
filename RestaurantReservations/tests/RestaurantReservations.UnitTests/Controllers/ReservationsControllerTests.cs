a8c16a6796c099f6a900d5a360c1f5db35272332
Pedro Morgado <1220492@isep.ipp.pt>
2026-01-23 01:06:15 +0000

    Remover bin/ e obj/ e atualizado ReservationsControllerTests.cs
    - Removidos os ficheiros bin/ e obj/.
    - Corrigido o ficheiro ReservationsControllerTests.cs pois dois testes estavam a falhar.

--- RestaurantReservations/tests/RestaurantReservations.UnitTests/Controllers/ReservationsControllerTests.cs
+++ RestaurantReservations/tests/RestaurantReservations.UnitTests/Controllers/ReservationsControllerTests.cs
@@ -3,8 +3,6 @@ using Moq;
 using RestaurantReservations.API.Controllers;
 using RestaurantReservations.API.DTOs;
 using RestaurantReservations.API.Services;
-using System;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace RestaurantReservations.UnitTests.Controllers
@@ -24,65 +22,73 @@ namespace RestaurantReservations.UnitTests.Controllers
         public async Task Post_Should_Return_Created_Reservation()
         {
             // Arrange
-            var dto = new CreateReservationDto
+            var createDto = new CreateReservationDto
             {
-                CustomerName = "Test Customer",
-                ReservationDate = DateTime.Today.AddDays(1),
-                ReservationTime = TimeSpan.FromHours(19),
+                CustomerName = "Cliente Teste",
                 TableNumber = 1,
-                NumberOfPeople = 2
+                ReservationDate = DateTime.Now.AddDays(1),
+                ReservationTime = new TimeSpan(19, 0, 0),
+                NumberOfPeople = 4
             };
 
             var expectedReservation = new ReservationDto
             {
                 Id = 1,
-                CustomerName = "Test Customer",
-                ReservationDate = DateTime.Today.AddDays(1),
-                ReservationTime = TimeSpan.FromHours(19),
+                CustomerName = "Cliente Teste",
                 TableNumber = 1,
-                NumberOfPeople = 2
+                ReservationDate = DateTime.Now.AddDays(1),
+                ReservationTime = new TimeSpan(19, 0, 0),
+                NumberOfPeople = 4
             };
 
-            _mockService.Setup(s => s.CreateReservationAsync(dto))
+            _mockService.Setup(s => s.CreateReservationAsync(createDto))
                 .ReturnsAsync(expectedReservation);
 
             // Act
-            var result = await _controller.CreateReservation(dto) as CreatedAtActionResult;
+            var actionResult = await _controller.CreateReservation(createDto);
+            var createdAtResult = actionResult.Result as CreatedAtActionResult;
 
             // Assert
-            Assert.NotNull(result);
-            var returnValue = Assert.IsType<ReservationDto>(result.Value);
-            Assert.Equal(1, returnValue.Id);
+            Assert.NotNull(createdAtResult);
+            Assert.Equal(201, createdAtResult.StatusCode);
+            Assert.Equal(expectedReservation, createdAtResult.Value);
         }
 
         [Fact]
         public async Task Get_Should_Return_Ok_With_Reservations()
         {
             // Arrange
-            var reservations = Array.Empty<ReservationDto>();
+            var reservations = new List<ReservationDto>
+            {
+                new() { Id = 1, CustomerName = "Cliente Teste", TableNumber = 1 },
+                new() { Id = 2, CustomerName = "Cliente Teste2", TableNumber = 2 }
+            };
+
             _mockService.Setup(s => s.GetAllReservationsAsync())
                 .ReturnsAsync(reservations);
 
             // Act
-            var result = await _controller.GetAllReservations(null) as OkObjectResult;
+            var actionResult = await _controller.GetReservations();
+            var okResult = actionResult.Result as OkObjectResult;
 
             // Assert
-            Assert.NotNull(result);
-            Assert.Equal(reservations, result.Value);
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(reservations, okResult.Value);
         }
 
         [Fact]
         public async Task GetById_Should_Return_NotFound_For_Invalid_Id()
         {
             // Arrange
-            _mockService.Setup(s => s.GetReservationByIdAsync(999))
+            _mockService.Setup(s => s.GetReservationByIdAsync(It.IsAny<int>()))
                 .ReturnsAsync((ReservationDto?)null);
 
             // Act
-            var result = await _controller.GetReservation(999);
+            var actionResult = await _controller.GetReservation(999);
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(result);
+            Assert.IsType<NotFoundResult>(actionResult.Result);
         }
     }
 }
